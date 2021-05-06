896676a06f78e99927c4d627d85efb6d0f906e27
Ilya-Ilya <levchenkoilja@gmail.com>
2021-05-06 21:07:02 +0300

    Create DB and index.php

---
+++ config/connect.php
@@ -0,0 +1,7 @@
+<?php
+$connect = mysqli_connect('localhost', 'root', 'root', 'bd_crud');
+if (!$connect) {
+    die ('Error connect to database!');
+}
+
+
---
+++ db_reprository/create.php
@@ -0,0 +1,4 @@
+<?php
+
+//Соединение с базой данных из соседней папки
+require_once '../config/connect.php';
